d282fe6b55db356a8ca8f6e7aef7d189e94da87c
skyangelsUA <dkskyangels@gmail.com>
2022-09-12 23:18:35 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,3 +9,21 @@ const btnStop = document.querySelector('button[data-stop]');
 
 let log = null;
 
+function goInt() {
+  log = setInterval(() => { bodyColor.style.backgroundColor = getRandomHexColor(); },
+      1000,
+      1000,
+  );
+    btnStart.setAttribute('disabled', true);
+    console.log(log)
+};
+
+
+function clearInt() {
+    clearInterval(log);
+    btnStart.removeAttribute("disabled");
+};
+
+btnStart.addEventListener('click', goInt);
+btnStop.addEventListener('click', clearInt);
+
