554233ea9e8af303c05ec5b730af817ed3e9b147
skyangelsUA <dkskyangels@gmail.com>
2022-09-12 23:30:35 +0300

    Start 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,18 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { convertMs } from './utils/convertMs';
+import { addLeadingZero } from './utils/addLeadingZero';
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
