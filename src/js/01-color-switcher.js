2ccf7d361372aacbb095a4e85c4e8a0c1fa65e38
skyangelsUA <dkskyangels@gmail.com>
2022-09-12 22:51:35 +0300

    new

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
 
+import { getRandomHexColor } from "./utils/getRandomHexColor";
+
+const bodyColor = document.querySelector("body");
+
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let log = null;
+
