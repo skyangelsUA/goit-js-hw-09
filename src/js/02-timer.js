6c7e407d6400c23700192a44f3b695afc93cd899
skyangelsUA <dkskyangels@gmail.com>
2022-09-14 17:29:26 +0300

    Alpha 1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,12 +6,12 @@ import { convertMs } from './utils/convertMs';
 import { addLeadingZero } from './utils/addLeadingZero';
 
 
-const calendar = document.querySelector();
-const btnStart = document.querySelector();
-const seconds = document.querySelector();
-const minutes = document.querySelector();
-const hours = document.querySelector();
-const days = document.querySelector();
+const calendar = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('button');
+const seconds = document.querySelector('[data-seconds]');
+const minutes = document.querySelector('[data-minutes]');
+const hours = document.querySelector('[data-hours]');
+const days = document.querySelector('[data-days]');
 
 
 
@@ -26,6 +26,40 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    console.log(selectedDates);
+    btnStart.removeAttribute('disable');
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure(`Виберіть дату в майбутньому!`);
+      btnStart.setAttribute('disabled', true);
+    }
+
+    btnStart.addEventListener('click', () => {
+      const deadLine = selectedDates[0].getTime();
+      let timer = null;
+      function downTimer() {
+        const dataDiff = convertMs(deadLine - Data.now());
+        const { days, hours, minutes, seconds } = dataDiff;
+        
+        if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+          clearInterval(timer);
+          btnStart.setAttribute('disabled', false);
+          calendar.setAttribute('disabled', false);
+        }
+
+        const timerS = addLeadingZero(seconds);
+        const timerM = addLeadingZero(minutes);
+        const timerH = addLeadingZero(hours);
+        const timerD = addLeadingZero(days);
+
+
+        seconds.innerHTML = timerS;
+        minutes.innerHTML = timerM;
+        hours.innerHTML = timerH;
+        days.innerHTML = timerD;
+      }
+      downTimer();
+      timer = setInterval(downTimer, 1000);
+    });
   },
 };
+const fp = flatpickr(calendarInput, options);
