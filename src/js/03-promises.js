be74772e310b06e79be4ca675f1d559c6a27cdb8
skyangelsUA <dkskyangels@gmail.com>
2022-09-14 21:32:26 +0300

    Alpha 3.1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", submit);
+
+function submit(event) {
+  event.preventDefault();
+  const data = {
+    delay: Number(form.elements.delay.value),
+    step: Number(form.elements.step.value),
+    amount: Number(form.elements.amount.value),
+  };
+  console.log(data);
+  promiseNotify(data);
+  event.currentTarget.reset();
+}
+
+function promiseNotify({ delay, step, amount }) {
+  for (let i = 1; i <= amount; i += 1) {
+    console.log(delay, step, amount, i);
+    createPromise(i, delay).then(({ position, delay }) => {
+      Notify.success(`Виконаний проміс ${position} за ${delay}мілісекунд`);
+    });
+    delay += step;
+  }
+}
+
+
+
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) = 
+  )
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
